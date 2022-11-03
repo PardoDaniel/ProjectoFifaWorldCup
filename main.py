da5c6ea327c9714d646b96bffa0a1375057cd2d1
Daniel Pardo <dani-pardo@hotmail.com>
2022-11-03 00:55:41 -0600

    Grafica de cuantos combeacks hubo por año

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot
+
+df = pd.read_csv('WorldCupMatches.csv')
+
+df = df[['Year','Stadium', 'City','Home Team Name','Home Team Goals','Away Team Goals','Away Team Name','Half-time Home Goals','Half-time Away Goals']]
+conditionsWin = [(df['Home Team Goals']>df['Away Team Goals']),
+                (df['Away Team Goals' ]> df['Home Team Goals']),
+                (df['Home Team Goals'] == df['Away Team Goals'])
+              ]
+valuesWin= [df['Home Team Name'],df['Away Team Name'],'Tie']
+df['Winner'] = np.select(conditionsWin, valuesWin)
+
+conditionsLeading = [(df['Half-time Home Goals']>df['Half-time Away Goals']),
+                (df['Half-time Away Goals' ]> df['Half-time Home Goals']),
+                (df['Half-time Home Goals'] == df['Half-time Away Goals'])
+              ]
+
+valuesLeading =[df['Home Team Name'],df['Away Team Name'],'Tie']
+
+df['Leading at Half-time'] = np.select(conditionsLeading,valuesLeading)
+
+conditionsComeback = [(df['Winner'] == df['Leading at Half-time']),
+                (df['Leading at Half-time' ] == 'Tie'),
+                (df['Winner'] != df['Leading at Half-time'])
+              ]
+valuesComeback = ['False','False','True']
+
+df['Comeback after Half-time'] = np.select(conditionsComeback,valuesComeback)
+
+
+
+dfComebacksByYear = df.groupby('Year')['Comeback after Half-time'].apply(lambda x: (x=='True').sum()).reset_index(name='count')
+
+fig, ax = plot.subplots()
+ax.plot(dfComebacksByYear['Year'],dfComebacksByYear['count'], linewidth= 2.0)
+plot.show()
+print(dfComebacksByYear)
